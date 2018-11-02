1fb83a8f63bb2865c20bf23f8684f3985e372ac2
and00000000 <33897529+and00000000@users.noreply.github.com>
2018-11-02 19:04:38 +0000

    Update TransactionTest.java
    Checked properties of transactions

--- src/test/java/com/abc/TransactionTest.java
+++ src/test/java/com/abc/TransactionTest.java
@@ -1,13 +1,25 @@
 package com.abc;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
 import static org.junit.Assert.assertTrue;
 
 public class TransactionTest {
+    
+        private static final double DOUBLE_DELTA = 1e-15;
+
     @Test
     public void transaction() {
-        Transaction t = new Transaction(5);
+        Transaction t = new Transaction(20,Transaction.DEPOSIT_FROM_BANK);
+        
+        t.getAmount();
+        
         assertTrue(t instanceof Transaction);
+        System.out.println(t.getTransactionDate());
+        assertEquals(20, t.getAmount(),DOUBLE_DELTA);
+        assertEquals("DEPOSIT FROM BANK", t.getTransactionType());
+        assertTrue(t instanceof Transaction);
+        
     }
 }
